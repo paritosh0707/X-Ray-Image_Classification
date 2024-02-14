ed22a185d5a7bbdf35023591fa9ca551ab2ed947
Paritosh Sharma <paritoshsharma0707@gmail.com>
2024-02-15 01:38:58 +0530

    data ingestion code skeleton added

---
+++ src/Xray/components/data_ingestion.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from Xray.utils.exception import CustomException
+
+
+class DataIngestion:
+    def __init__(self):
+        pass
+
+    def get_data_from_s3(self):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def intitiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+    
