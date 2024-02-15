fe604c716af34351de832f889ef5d81e8e2612d7
Paritosh Sharma <paritoshsharma0707@gmail.com>
2024-02-15 15:05:17 +0530

    updated data_ingestion component

--- src/Xray/components/data_ingestion.py
+++ src/Xray/components/data_ingestion.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from Xray.entity.config_entity import DataIngestionConfig
+from Xray.entity.artifact_entity import DataIngestionArtifact
 from Xray.cloud_storage.s3_operation import S3Openration
 from Xray.utils.exception import CustomException
 from Xray.utils.logger import logging
@@ -12,7 +13,7 @@ class DataIngestion:
         self.data_ingestion_config = data_ingestion_config
         self.s3 = S3Openration()
 
-    def get_data_from_s3(self):
+    def get_data_from_s3(self)-> None:
         try:
             logging.info("Entered the get_data_from_s3 method of the DataIngestion class")
             self.s3.sync_folder_from_s3(
@@ -24,9 +25,16 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-    def intitiate_data_ingestion(self):
+    def intitiate_data_ingestion(self)-> DataIngestionArtifact:
         try:
-            pass
+            logging.info("Entering intitiate_data_ingestion method of the DataIngestion class")
+            self.get_data_from_s3()
+            data_ingestion_artifact: DataIngestionArtifact = DataIngestionArtifact(
+                train_file_path=self.data_ingestion_config.train_data_path,
+                test_file_path=self.data_ingestion_config.test_data_path
+            )
+            logging.info("Exited the initiate data ingestion method of the DataIngestion class")
+            return data_ingestion_artifact
         except Exception as e:
             raise CustomException(e,sys)
     
