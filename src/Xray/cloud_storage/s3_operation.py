95db27eed96f8d57663a3b31db7410f3be7ede4c
Paritosh Sharma <paritoshsharma0707@gmail.com>
2024-02-15 14:42:37 +0530

    implemented S3 operations

---
+++ src/Xray/cloud_storage/s3_operation.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from Xray.utils.exception import CustomException
+
+class S3Openration:
+    def __init__(self):
+        pass
+
+    def sync_folder_to_s3(self, folder: str, bucket_name: str, bucket_folder_name: str) -> None:
+        try:
+            command: str = (
+                f"aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}"
+            )
+            os.system(command)
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def sync_folder_from_s3(self, folder: str, bucket_name: str, bucket_folder_name: str) -> None:
+        try:
+            command: str = (
+                f"aws s3 sync s3://{bucket_name}/{bucket_folder_name}/{folder}"
+            )
+            os.system(command)
+        except Exception as e:
+            raise CustomException(e,sys)
